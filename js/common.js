44aff66d078a0bb7ea75649b60c2b24ce5950baf
cli628 <cli6028@gmail.com>
2026-02-23 21:38:38 +0900

    change

--- js/common.js
+++ js/common.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  AOS.init({
-    disable: false,                //  AOS 끄지 않기! → 애니메이션 작동하게 두기
-    startEvent: 'DOMContentLoaded', //  HTML이 다 불러와지면 바로 AOS 시작!
-    initClassName: 'aos-init',      //  AOS가 준비됐다는 표시 클래스 (자동 붙음)
-    animatedClassName: 'aos-animate', //  애니메이션이 실행될 때 붙는 클래스 이름
-    useClassNames: false,           //  HTML에 data-aos 값 그대로 클래스 안 붙이기
-    disableMutationObserver: false, //  새로 생긴 요소도 자동 감시해서 애니메이션 적용
-    debounceDelay: 50,              //  창 크기 바꿀 때 0.05초 기다렸다 계산 (너무 자주 안 하게)
-    throttleDelay: 99               //  스크롤할 때 0.099초마다 한 번씩 체크 (성능 좋게!)
+
+   // jsap
+  document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger)
+  // gsap code here!
+
+  
   });
+
+
+
+
 
 
   const header = document.querySelector('header');
--- js/main.js
+++ js/main.js
@@ -1,21 +1,140 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+  /* jsap */
+ document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger)
+  // gsap code here!
+
+ });
+
+  /* 메인비주얼 */
 const _SELECTOR={
   btn:".main_visual>.inner>.bottom_btn",
   main:".main_visual>.inner>.slide>.top_sw",
 };
 
-const _BTN_OPT={
+let _BTN_OPT={
   slidesPerView:5,
   watchSlidesProgress:true,
 };
 
-const swiperBtn=new Swiper(_SELECTOR.btn,_BTN_OPT);
+let swiperBtn=new Swiper(_SELECTOR.btn,_BTN_OPT);
 
-const swiperMain=new Swiper(_SELECTOR.main,{
+let swiperMain=new Swiper(_SELECTOR.main,{
   loop:true,
   thumbs:{swiper:swiperBtn},
 });
 
+
+/* 서브 부분마우스 호버시 마우스 따라 원 움직임 */
+document.querySelectorAll('.sub').forEach((wrap) => {
+
+  const ballsPl = wrap.querySelectorAll('.main_visual .sub .num_txt, .num_txt p');
+  const ballsMn = wrap.querySelectorAll('.main_visual .sub .txt>h3, .sub .txt p,.sub .txt');
+  const ballsRo = wrap.querySelectorAll('.main_visual .sub .ball_all');
+  wrap.addEventListener('mousemove', (e) => {
+
+    const rect = wrap.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const moveX = (x - centerX) / 120;
+    const moveY = (y - centerY) / 120;
+
+    ballsPl.forEach((ball, i) => {
+      const depth = (i + 1) * 8;
+      ball.style.transform =
+        `translate(${moveX * depth/9}px, ${moveY * depth/9}px)`;
+    });
+
+    ballsMn.forEach((ball, i) => {
+      const depth = (i + 1) * 3;
+      ball.style.transform =
+        `translate(${moveX * depth/7}px, ${moveY * depth/7}px)`;
+    });
+    ballsRo.forEach((ball, i) => {
+      const depth = (i + 1) * 3;
+      ball.style.transform =  `rotateX(${moveX * depth}deg) rotateY(${moveY*depth}deg)`;
+    });
+  });
+
+  wrap.addEventListener('mouseleave', () => {
+
+    balls.forEach((ball) => {
+      ball.style.transform = `translate(0,0)`;
+    });
+
+  });
+
+});
+
+/* 숫자 올라가는 카운트 효과 */
+function countUp(element, target, duration = 2000) {
+  const start = target * 0.65; // 65%부터 시작
+  const startTime = performance.now();
+
+  function update(now) {
+    const progress = Math.min((now - startTime) / duration, 1);
+    const value = Math.floor(start + (target - start) * progress);
+
+    element.textContent =
+      value.toLocaleString() + "원";
+
+    if (progress < 1) {
+      requestAnimationFrame(update);
+    }
+  }
+
+  requestAnimationFrame(update);
+}
+
+const numberEl = document.querySelector('.num_txt strong');
+const targetNumber = 166439507143;
+
+countUp(numberEl, targetNumber, 2000);
+
+/* 메인 카드 선택부분 */
+const swiper = new Swiper(".card_all", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "2",
+  loop:true,
+  navigation: {
+        nextEl: ".swiper_button_next",
+        prevEl: ".swiper_button_prev",
+      },
+  coverflowEffect: {
+    rotate: -30,
+    // stretch: 1,
+    // depth: 1,
+    // modifier: 1,
+  },
+  scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+
+});
+const pick_selector={
+  btn:"swiper_button_next, swiper_button_prev",
+  main:".pick>.inner card_all swiper-slide",
+  
+};
+
+const pick_btn_opt={
+  watchSlidesProgress:true,
+};
+const pickBtn = new Swiper(pick_selector.btn, pick_btn_opt);
+
+const pickMain=new Swiper(pick_selector.main, {
+  loop:true,
+  thumbs:{swiper:pick_btn_opt},
+});
+    
 
 
 //marquee 
